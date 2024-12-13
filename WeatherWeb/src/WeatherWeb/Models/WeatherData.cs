256b41f75f5848f00f26385fe0c33f8063865fdf
Sourav Kundu <skundu.dev@gmail.com>
2024-12-12 22:20:10 -0600

    added weather web app

---
+++ WeatherWeb/src/WeatherWeb/Models/WeatherData.cs
@@ -0,0 +1,9 @@
+namespace WeatherWeb.Models
+{
+    public class WeatherData
+    {
+        public int Temperature { get; set; }
+        public string Location { get; set; }
+        public string Unit { get; set; }
+    }
+}
---
+++ WeatherWeb/src/WeatherWeb/Services/WeatherService.cs
@@ -0,0 +1,47 @@
+using WeatherWeb.Models;
+
+namespace WeatherWeb.Services
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _client;
+        private readonly ILogger<WeatherService> _logger;
+
+        public WeatherService(HttpClient client, ILogger<WeatherService> logger)
+        {
+            _client = client;
+            _logger = logger;
+        }
+
+        public async Task<WeatherData?> GetWeatherForLocation(string location)
+        {
+            try
+            {
+                var requestUrl = $"http://weatherapi:8080/weather/{location}";
+                _logger.LogInformation($"Attempting to call API at: {requestUrl}");
+
+                var response = await _client.GetAsync(requestUrl);
+                
+                _logger.LogInformation($"API Response Status: {response.StatusCode}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    _logger.LogInformation($"API Response content: {content}");
+                    return await response.Content.ReadFromJsonAsync<WeatherData>();
+                }
+                
+                var errorContent = await response.Content.ReadAsStringAsync();
+                _logger.LogError("Error fetching weather: Status: {StatusCode}, Content: {Content}", 
+                    response.StatusCode, 
+                    errorContent);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception while calling weather service");
+                throw;
+            }
+        }
+    }
+}
