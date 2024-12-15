f412fb39277b554a13b13bf567818b526204d48e
Sourav Kundu <skundu.dev@gmail.com>
2024-12-15 12:00:16 -0600

    corrected port mapping #12

--- infrastructure/security_group.tf
+++ infrastructure/security_group.tf
@@ -48,8 +48,8 @@ resource "aws_security_group" "container_sg" {
 resource "aws_security_group_rule" "ingress_container" {
   description              = "allow traffic into the containers from the vpc"
   type                     = "ingress"
-  from_port                = 8080
-  to_port                  = 8080
+  from_port                = 80
+  to_port                  = 80
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.custom_sg.id
   security_group_id        = aws_security_group.container_sg.id
