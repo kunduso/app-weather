9606e42ede1480a9ead39f7ed36b1a88b234ff15
sourav kundu <kunduso@amazon.com>
2025-09-23 11:46:03 -0700

    added healthcheck grace

--- application/WeatherWeb/deploy/ecs_service.tf
+++ application/WeatherWeb/deploy/ecs_service.tf
@@ -1,10 +1,11 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
 resource "aws_ecs_service" "service" {
-  name                 = "${var.name}-web"
-  cluster              = local.infra_output["aws_ecs_cluster_id"]
-  task_definition      = aws_ecs_task_definition.web_app.arn
-  desired_count        = 2
-  force_new_deployment = true
+  name                              = "${var.name}-web"
+  cluster                           = local.infra_output["aws_ecs_cluster_id"]
+  task_definition                   = aws_ecs_task_definition.web_app.arn
+  desired_count                     = 2
+  force_new_deployment              = true
+  health_check_grace_period_seconds = 120
   load_balancer {
     target_group_arn = local.infra_output["aws_lb_target_group_arn"]
     container_name   = "web"
