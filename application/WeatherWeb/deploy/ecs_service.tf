ea68a2f00aeaeb6a9d6bc411b836424ca5da9a0f
sourav kundu <kunduso@amazon.com>
2025-10-03 08:23:25 -0500

    update desired count for high availability

--- application/WeatherWeb/deploy/ecs_service.tf
+++ application/WeatherWeb/deploy/ecs_service.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "service" {
   name                              = "${var.name}-web"
   cluster                           = local.infra_output["aws_ecs_cluster_id"]
   task_definition                   = aws_ecs_task_definition.web_app.arn
-  desired_count                     = 1
+  desired_count                     = 2
   force_new_deployment              = true
   health_check_grace_period_seconds = 120
   load_balancer {
